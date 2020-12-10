a3bd53cacc974c9cd3453e76a5bb65d04c117642
Mira0Kit <kitomura@bk.ru>
2020-12-10 23:46:26 +0300

    1

---
+++ src/app/shared/models/mworker.model.ts
@@ -0,0 +1,17 @@
+export interface Mworker {
+    id?: number;
+    name: string;
+    surname: string;
+    patronymic: string;
+    number: string;
+    email: string;
+    birthdate: string;
+    type: number;
+}
+
+export enum MworkerType {
+    it,
+    sales,
+    delivery,
+    legal
+}
